ee7287ed4cbc6f85f5801950c8056fe8c00b211d
Aaron <877406170@qq.com>
2026-05-06 23:07:42 +0800

    fix: bundle prisma client for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,17 @@ import type { NextConfig } from "next";
 
 import("@opennextjs/cloudflare").then((m) => m.initOpenNextCloudflareForDev());
 
+type WebpackConfig = Parameters<NonNullable<NextConfig["webpack"]>>[0];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config: WebpackConfig) {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
